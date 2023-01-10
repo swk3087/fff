ccb467978792469ae82b5197bd305ebcf0de9f9b
swk3087 <89524975+swk3087@users.noreply.github.com>
2023-01-10 18:21:15 +0900

    Update time.php

--- time.php
+++ time.php
@@ -1,4 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 $timezones = ["Asia/Seoul", "America/New_York"];
 echo json_encode($timezones);
 ?>
+</body>
+</html>
